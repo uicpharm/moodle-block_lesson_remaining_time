0c61cedd2eba636021a1aac094031f8eb5efe731
Josh Curtiss <josh@curtiss.me>
2025-06-24 15:58:45 -0500

    chore: Bump version

--- src/version.php
+++ src/version.php
@@ -3,5 +3,5 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_lesson_remaining_time';
-$plugin->version = 2024021511;
+$plugin->version = 2025031208;
 $plugin->requires = 2020061512.05;
